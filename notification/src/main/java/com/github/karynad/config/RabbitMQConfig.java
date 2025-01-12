0486cea6d15006ffbac11a34a545062cdd518aa5
Karyna <karyna.dzenisevich@gmail.com>
2025-01-12 17:58:49 +0100

    Update deploy k8s for notification service

--- notification/src/main/java/com/github/karynad/config/RabbitMQConfig.java
+++ notification/src/main/java/com/github/karynad/config/RabbitMQConfig.java
@@ -1,10 +1,12 @@
 package com.github.karynad.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(name = "spring.rabbitmq.enabled", havingValue = "true", matchIfMissing = true)
 public class RabbitMQConfig {
 
     @Bean
